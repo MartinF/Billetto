e50015f7c9f13aaffc3abb9070bc2aa34d49e771
Martin From <from@pc.dk>
2013-05-20 23:18:20 +0200

    Added search feature. Logo moved to the left, search to the right.

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,14 +1,23 @@
 class EventController < ApplicationController
 
   def index
+    @search = params[:search]
+
     #add end date and show all that have not ended yet OR not started yet (if end date is not available) - or started + 1 hour or something
-    @events = Event.where('start >= ?', DateTime.now).limit(10).order("start ASC").group_by { |event|
+    query = Event.where('start >= ?', DateTime.now).limit(20).order("start ASC")
+
+    unless @search.blank?
+      # Only search in title for now
+      query = query.where("title LIKE ?", "%#{@search}%")
+    end
+
+    @events = query.group_by { |event|
       event.start.to_date
     }
   end
 
   def show
-    # Doesn't seem very secure ... !
+    # Doesn't seem very secure ... !?
     @event = Event.find(params[:id])
   end
 
